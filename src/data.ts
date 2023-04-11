ad1b3364159eef8e0dede76c8eca12a60c0f4071
Sebastian Pfliegel <sebastian.pfliegel@apollo.de>
2023-04-11 17:24:32 +0200

    Actual streetteam templates included

---
+++ src/data.ts
@@ -0,0 +1,237 @@
+export const teams = [
+  {
+    name: 'Bamberg',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Bamberg.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Berlin',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Berlin.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Bingen',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Bingen.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Bremen',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Bremen.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Coburg',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Coburg.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Dortmund',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Dortmund.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Dresden',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Dresden.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Düsseldorf',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Duesseldorf.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Duisburg',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Duisburg.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Essen',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Essen.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Göttingen',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Goettingen.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Hamburg',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Hamburg.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Hannover',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Hannover.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Heidelberg',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Heidelberg.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Kaiserslautern',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Kaiserslautern.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Karlsruhe',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Karlsruhe.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Kleve',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Kleve.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Köln/Bonn',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_KoelnBonn.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Konstanz',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Konstanz.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Leipzig',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Leipzig.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Lübeck',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Luebeck.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Lüneburg',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Lueneburg.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Memmingen',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Memmingen.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Nürnberg',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Nuernberg.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Osnabrück',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Osnabrueck.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Rhein-Main',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_RheinMain.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Rosenheim',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Rosenheim.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Rostock',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Rostock.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Saarbrücken',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Saarbruecken.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Siegen',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Siegen.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Stuttgart',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Stuttgart.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Tübingen',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Tuebingen.jpg',
+      import.meta.url
+    ).href
+  },
+  {
+    name: 'Wuppertal',
+    img: new URL(
+      './assets/templates/Aktionsankuendigung-Streetteam-1080x1350px_Wuppertal.jpg',
+      import.meta.url
+    ).href
+  }
+]
+
+export default {
+  teams
+}
